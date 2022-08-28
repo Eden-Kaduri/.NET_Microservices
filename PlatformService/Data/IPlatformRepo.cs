ca03ad3a204b5151d97ca858e47023fb21aef394
Eden Kaduri <edenkadouri@gmail.com>
2022-08-28 14:17:58 +0300

    Platform service scaffolding & Repo implementation

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,13 @@
+﻿using PlatformService.Models;
+using System.Collections.Generic;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform();
+        Platform GetPlatformById(int id);
+    }
+}
