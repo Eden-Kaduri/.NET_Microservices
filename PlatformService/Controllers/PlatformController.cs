995634dc687613bcf5bf0245e31d5d196a5770ef
Eden Kaduri <edenkadouri@gmail.com>
2022-08-31 14:59:26 +0300

    Dtos, Controller and prepDb
    Added a create & read DTO, created mapping, injected mock data into the db, and implemented the controller to handle the http requests.

---
+++ PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using System.Collections.Generic;
+
+namespace PlatformService.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly IPlatformRepo _repo;
+        private readonly IMapper _mapper;
+
+        public PlatformController(IPlatformRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms()
+        {
+            System.Console.WriteLine("-->Get all platforms");
+            var platforms = _repo.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            System.Console.WriteLine("-->Get platform with id "+ id);
+            var platform = _repo.GetPlatformById(id);
+            if(platform != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platform));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            System.Console.WriteLine("-->Creating new Platform");
+            var platformModel= _mapper.Map<Platform>(platformCreateDto);
+            _repo.CreatePlatform(platformModel);
+            _repo.SaveChanges();
+
+            var platformRead = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id=platformRead.Id },platformRead);
+        }
+    }
+}
